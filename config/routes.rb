a536836c64b1f37c125f1a524dab915108104d56
nithin s k <nsk254@gmail.com>
2022-10-05 01:05:23 +0530

    added about me routes

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,7 @@ Rails.application.routes.draw do
   #we are creating a separate controller to render out html and css components and pages details maybe to the user 
   root "pages#home"
 
+  #translates to get requests
+  get "about" , to:'pages#about'
+
 end
